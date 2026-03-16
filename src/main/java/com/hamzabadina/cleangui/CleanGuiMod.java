97548a0f12adf84474c5b3d6e8ebeb90f42baf9c
hamzabadina-hash <hamzabadina@gmail.com>
2026-03-16 14:01:35 +0000

    Update CleanGuiMod.java

--- src/main/java/com/hamzabadina/cleangui/CleanGuiMod.java
+++ src/main/java/com/hamzabadina/cleangui/CleanGuiMod.java
@@ -2,6 +2,7 @@ package com.hamzabadina.cleangui;
 
 import com.hamzabadina.cleangui.network.SwapAndHitPacket;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class CleanGuiMod implements ModInitializer {
     public static final String MOD_ID = "cleangui";
@@ -9,5 +10,10 @@ public class CleanGuiMod implements ModInitializer {
     @Override
     public void onInitialize() {
         SwapAndHitPacket.registerReceiver();
+
+        // Log when a player joins so server knows mod is active
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            System.out.println("[CleanGUI] " + handler.player.getName().getString() + " joined with Clean GUI mod.");
+        });
     }
 }
